d9c0098dd4634d2f0f45e0b26bf9b7c3e1473033
Ivan Gromov <summer.is.gone@gmail.com>
2016-11-06 22:48:56 +0500

    Added track info

---
+++ music/fill_mongo.py
@@ -0,0 +1,46 @@
+# -*- coding: utf-8 -*-
+import datetime
+import json
+import re
+import argparse
+from os.path import join
+from glob import glob
+from pymongo import MongoClient
+
+MONGO_CONNECTION = 'localhost:27017'
+MONGO_DBNAME = 'aerost8'
+MONGO_COLLECTION = 'issues'
+
+client = MongoClient(MONGO_CONNECTION)
+
+def process_track(line):
+    time, track_no, name = re.match('^([0-9.]+)\s(\d{1,3})\s?-{1,3}\s?(.*).mp3$', line).groups()
+    return {
+        'time': float(time),
+        'order': int(track_no),
+        'title': name
+    }
+
+def main(txt_folder):
+    collection = client[MONGO_DBNAME][MONGO_COLLECTION]
+    for filename in glob(join(txt_folder, '*.txt')):
+        with open(filename, 'r') as f:
+            content = f.read().decode('utf8')
+            issue, slug = re.search('(\d{3})-(.*)\.txt', filename).groups()
+            tracks = [process_track(t) for t in content.splitlines()]
+
+            collection.find_one_and_update({
+                'issue': int(issue)
+            }, {
+                '$set': {
+                    'tracks': tracks,
+                    'slug': slug
+                }
+            })
+            print '{0} updated'.format(issue)
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('folder', help='folder with txt files')
+    args = parser.parse_args()
+    main(args.folder)
---
+++ music/gen.py
@@ -0,0 +1,44 @@
+# -*- coding: utf-8 -*-
+from pymongo import MongoClient
+import datetime
+from os.path import join
+import argparse
+
+client = MongoClient()
+MONGO_CONNECTION = 'localhost:27017'
+MONGO_DBNAME = 'aerost8'
+MONGO_COLLECTION = 'issues'
+FILENAME = '{date}-{slug}.md'
+CONTENT = u"""---
+layout: post
+title: {title}
+issue: {issue}
+date: {date}
+track: {url}
+---
+
+{content}
+"""
+
+def main(out_folder):
+    client = MongoClient(MONGO_CONNECTION)
+    cur = client[MONGO_DBNAME][MONGO_COLLECTION]
+    for issue in cur.find({}):
+        data = dict(
+            date=issue['date'].strftime('%Y-%m-%d'),
+            slug=issue.get('slug', None),
+            title=issue.get('name', None),
+            url=issue.get('url', None),
+            issue=issue.get('issue', None),
+            content='\n\n'.join(issue['paragraphs'])
+        )
+        with open(join(out_folder, FILENAME.format(**data)), 'w') as f:
+            # print type(CONTENT.format(**data))
+            f.write(CONTENT.format(**data).encode('utf8'))
+        print data['title']
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('folder', help='out folder')
+    args = parser.parse_args()
+    main(args.folder)
